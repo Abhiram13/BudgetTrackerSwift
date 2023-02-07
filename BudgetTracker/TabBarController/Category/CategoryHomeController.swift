56b25e4a5f4ff443da31b1f562c517324b657c16
Abhiram Nagadi <40509745+Abhiram13@users.noreply.github.com>
2023-02-07 22:12:04 +0530

    added scroller in category home

--- BudgetTracker/TabBarController/Category/CategoryHomeController.swift
+++ BudgetTracker/TabBarController/Category/CategoryHomeController.swift
@@ -1,7 +1,18 @@
 import UIKit;
 
 class CategoryHomeController: UIViewController {
+    let scroller = UIScrollView();
+    
     override func viewDidLoad() {
         self.view.backgroundColor = .green;
+        
+        view.addSubview(scroller);
+        
+        scroller.translatesAutoresizingMaskIntoConstraints = false;
+        scroller.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true;
+        scroller.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor).isActive = true;
+        scroller.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor).isActive = true;
+        scroller.backgroundColor = .systemPink;
+        scroller.contentSize = CGSize(width: view.frame.width, height: 2000);
     }
 }
